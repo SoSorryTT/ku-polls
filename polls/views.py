e00ca4c8afc5d4314ba59958490f1f1b87e7c5bc
SoSorryTT <markpanya@gmail.com>
2021-09-05 00:42:33 +0700

    Commit tutorial django part 6 and move test in view to test.py and fig an error

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.template import context, loader
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.test import TestCase
 
 
 class IndexView(generic.ListView):
@@ -22,28 +23,6 @@ class DetailView(generic.DetailView):
 
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class QuestionDetailViewTests(TestCase):
-    def test_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future
-        returns a 404 not found.
-        """
-        future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
-    def test_past_question(self):
-        """
-        The detail view of a question with a pub_date in the past
-        displays the question's text.
-        """
-        past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
-        response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
 
 
 class ResultsView(generic.DetailView):
